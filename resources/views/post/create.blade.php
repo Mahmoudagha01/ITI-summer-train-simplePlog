905f7ca333d7d3f92c151ab6c925e5acd5a4567a
Mahmoud Yasser <moodyalagha20@gmail.com>
2021-08-22 10:34:56 +0200

    add bootsrap

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,18 +1,22 @@
 @extends('layouts.app')
-<form action="/posts" method="post">
+@section('content')
+
+<form action="/posts" method="post" style="margin: auto 25%">
     @csrf
-<input type="text" name="title" placeholder="title"><br>
-<input type="text" name="description" placeholder="description"><br>
-<select name="user_id" class="form-select form-select-sm w-25" >
+    <br>
+<input class="form-control w-75"  type="text" name="title" placeholder="title"><br>
+<input class="form-control w-75"  type="text" name="description" placeholder="description"><br>
+<select name="user_id" class="form-select form-select-sm w-75 " >
     @foreach ($users as $user)
     <option value="{{$user->id}}">{{$user->name}}</option>
     @endforeach
-</select>
-<input type="text" name="created_at" placeholder="Created at"><br>
-<input type="text" name="updated_at" placeholder="posted by"><br>
-<input type="submit" name="add">
+</select><br>
+<input class="form-control w-75" type="text" name="created_at" placeholder="Created at"><br>
+<input class="form-control  w-75" type="text" name="updated_at" placeholder="posted by"><br>
+<input class="form-control w-25" type="submit" name="add">
 </form>
 
+@endsection
 @if ($errors->any())
     <div class="alert alert-danger">
         <ul>
@@ -22,3 +26,4 @@
         </ul>
     </div>
 @endif
+
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -1,10 +1,14 @@
-<form action="posts/{{$post->id}}" method="post">
+@extends('layouts.app')
+@section('content')
+
+<form action="posts/{{$post->id}}" method="post" style="margin: auto 25%">
 
     @csrf
     @method('PUT')
-    <input type="text" name="title" placeholder="title"><br>
-    <input type="text" name="desc" placeholder="description"><br>
-    <input type="text" name="posted_by" placeholder="posted by"><br>
-    <input type="text" name="created_at" placeholder="Created at"><br>
-    <input type="submit" name="add">
+    <input class="form-control w-75" type="text" name="title" placeholder="title"><br>
+    <input class="form-control w-75" type="text" name="desc" placeholder="description"><br>
+    <input class="form-control w-75" type="text" name="posted_by" placeholder="posted by"><br>
+    <input class="form-control w-75" type="text" name="created_at" placeholder="Created at"><br>
+    <input class="form-control w-25" type="submit" name="add">
     </form>
+    @endsection
